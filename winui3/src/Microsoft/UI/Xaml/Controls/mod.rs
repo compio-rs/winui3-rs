471adfcd5cdab3df327671cc1a4ca57ea416e91d
王宇逸 <Strawberry_Str@hotmail.com>
2025-09-13 23:04:29 +0800

    More event args types

--- winui3/src/Microsoft/UI/Xaml/Controls/mod.rs
+++ winui3/src/Microsoft/UI/Xaml/Controls/mod.rs
@@ -8541,6 +8541,23 @@ impl ComboBox {
             .ok()
         }
     }
+    pub fn TextSubmitted<P0>(&self, handler: P0) -> windows_core::Result<i64>
+    where
+        P0: windows_core::Param<
+            windows::Foundation::TypedEventHandler<ComboBox, ComboBoxTextSubmittedEventArgs>,
+        >,
+    {
+        let this = self;
+        unsafe {
+            let mut result__ = core::mem::zeroed();
+            (windows_core::Interface::vtable(this).TextSubmitted)(
+                windows_core::Interface::as_raw(this),
+                handler.param().abi(),
+                &mut result__,
+            )
+            .map(|| result__)
+        }
+    }
     pub fn RemoveTextSubmitted(&self, token: i64) -> windows_core::Result<()> {
         let this = self;
         unsafe {
@@ -14518,6 +14535,62 @@ impl windows_core::RuntimeName for ComboBoxItem {
 unsafe impl Send for ComboBoxItem {}
 #[cfg(all(feature = "UI_Composition", feature = "UI_Xaml_Controls_Primitives"))]
 unsafe impl Sync for ComboBoxItem {}
+#[repr(transparent)]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct ComboBoxTextSubmittedEventArgs(windows_core::IUnknown);
+windows_core::imp::interface_hierarchy!(
+    ComboBoxTextSubmittedEventArgs,
+    windows_core::IUnknown,
+    windows_core::IInspectable
+);
+impl ComboBoxTextSubmittedEventArgs {
+    pub fn Text(&self) -> windows_core::Result<windows_core::HSTRING> {
+        let this = self;
+        unsafe {
+            let mut result__ = core::mem::zeroed();
+            (windows_core::Interface::vtable(this).Text)(
+                windows_core::Interface::as_raw(this),
+                &mut result__,
+            )
+            .map(|| core::mem::transmute(result__))
+        }
+    }
+    pub fn Handled(&self) -> windows_core::Result<bool> {
+        let this = self;
+        unsafe {
+            let mut result__ = core::mem::zeroed();
+            (windows_core::Interface::vtable(this).Handled)(
+                windows_core::Interface::as_raw(this),
+                &mut result__,
+            )
+            .map(|| result__)
+        }
+    }
+    pub fn SetHandled(&self, value: bool) -> windows_core::Result<()> {
+        let this = self;
+        unsafe {
+            (windows_core::Interface::vtable(this).SetHandled)(
+                windows_core::Interface::as_raw(this),
+                value,
+            )
+            .ok()
+        }
+    }
+}
+impl windows_core::RuntimeType for ComboBoxTextSubmittedEventArgs {
+    const SIGNATURE: windows_core::imp::ConstBuffer =
+        windows_core::imp::ConstBuffer::for_class::<Self, IComboBoxTextSubmittedEventArgs>();
+}
+unsafe impl windows_core::Interface for ComboBoxTextSubmittedEventArgs {
+    type Vtable = <IComboBoxTextSubmittedEventArgs as windows_core::Interface>::Vtable;
+    const IID: windows_core::GUID =
+        <IComboBoxTextSubmittedEventArgs as windows_core::Interface>::IID;
+}
+impl windows_core::RuntimeName for ComboBoxTextSubmittedEventArgs {
+    const NAME: &'static str = "Microsoft.UI.Xaml.Controls.ComboBoxTextSubmittedEventArgs";
+}
+unsafe impl Send for ComboBoxTextSubmittedEventArgs {}
+unsafe impl Sync for ComboBoxTextSubmittedEventArgs {}
 #[cfg(feature = "UI_Composition")]
 #[repr(transparent)]
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -23125,9 +23198,11 @@ impl windows_core::RuntimeType for IComboBox {
     const SIGNATURE: windows_core::imp::ConstBuffer =
         windows_core::imp::ConstBuffer::for_interface::<Self>();
 }
+#[cfg(all(feature = "UI_Composition", feature = "UI_Xaml_Controls_Primitives"))]
 impl windows_core::RuntimeName for IComboBox {
     const NAME: &'static str = "Microsoft.UI.Xaml.Controls.IComboBox";
 }
+#[cfg(all(feature = "UI_Composition", feature = "UI_Xaml_Controls_Primitives"))]
 pub trait IComboBox_Impl: windows_core::IUnknownImpl {
     fn IsDropDownOpen(&self) -> windows_core::Result<bool>;
     fn SetIsDropDownOpen(&self, value: bool) -> windows_core::Result<()>;
@@ -23163,8 +23238,15 @@ pub trait IComboBox_Impl: windows_core::IUnknownImpl {
         handler: windows_core::Ref<windows::Foundation::EventHandler<windows_core::IInspectable>>,
     ) -> windows_core::Result<i64>;
     fn RemoveDropDownOpened(&self, token: i64) -> windows_core::Result<()>;
+    fn TextSubmitted(
+        &self,
+        handler: windows_core::Ref<
+            windows::Foundation::TypedEventHandler<ComboBox, ComboBoxTextSubmittedEventArgs>,
+        >,
+    ) -> windows_core::Result<i64>;
     fn RemoveTextSubmitted(&self, token: i64) -> windows_core::Result<()>;
 }
+#[cfg(all(feature = "UI_Composition", feature = "UI_Xaml_Controls_Primitives"))]
 impl IComboBox_Vtbl {
     pub const fn new<Identity: IComboBox_Impl, const OFFSET: isize>() -> Self {
         unsafe extern "system" fn IsDropDownOpen<Identity: IComboBox_Impl, const OFFSET: isize>(
@@ -23496,6 +23578,23 @@ impl IComboBox_Vtbl {
                 IComboBox_Impl::RemoveDropDownOpened(this, token).into()
             }
         }
+        unsafe extern "system" fn TextSubmitted<Identity: IComboBox_Impl, const OFFSET: isize>(
+            this: *mut core::ffi::c_void,
+            handler: *mut core::ffi::c_void,
+            result__: *mut i64,
+        ) -> windows_core::HRESULT {
+            unsafe {
+                let this: &Identity =
+                    &*((this as *const *const ()).offset(OFFSET) as *const Identity);
+                match IComboBox_Impl::TextSubmitted(this, core::mem::transmute_copy(&handler)) {
+                    Ok(ok__) => {
+                        result__.write(core::mem::transmute_copy(&ok__));
+                        windows_core::HRESULT(0)
+                    }
+                    Err(err) => err.into(),
+                }
+            }
+        }
         unsafe extern "system" fn RemoveTextSubmitted<
             Identity: IComboBox_Impl,
             const OFFSET: isize,
@@ -23545,7 +23644,7 @@ impl IComboBox_Vtbl {
             RemoveDropDownClosed: RemoveDropDownClosed::<Identity, OFFSET>,
             DropDownOpened: DropDownOpened::<Identity, OFFSET>,
             RemoveDropDownOpened: RemoveDropDownOpened::<Identity, OFFSET>,
-            TextSubmitted: 0,
+            TextSubmitted: TextSubmitted::<Identity, OFFSET>,
             RemoveTextSubmitted: RemoveTextSubmitted::<Identity, OFFSET>,
         }
     }
@@ -23637,6 +23736,13 @@ pub struct IComboBox_Vtbl {
     ) -> windows_core::HRESULT,
     pub RemoveDropDownOpened:
         unsafe extern "system" fn(*mut core::ffi::c_void, i64) -> windows_core::HRESULT,
+    #[cfg(all(feature = "UI_Composition", feature = "UI_Xaml_Controls_Primitives"))]
+    pub TextSubmitted: unsafe extern "system" fn(
+        *mut core::ffi::c_void,
+        *mut core::ffi::c_void,
+        *mut i64,
+    ) -> windows_core::HRESULT,
+    #[cfg(not(all(feature = "UI_Composition", feature = "UI_Xaml_Controls_Primitives")))]
     TextSubmitted: usize,
     pub RemoveTextSubmitted:
         unsafe extern "system" fn(*mut core::ffi::c_void, i64) -> windows_core::HRESULT,
@@ -23944,6 +24050,106 @@ pub struct IComboBoxStatics_Vtbl {
     TextProperty: usize,
     TextBoxStyleProperty: usize,
     DescriptionProperty: usize,
+}
+windows_core::imp::define_interface!(
+    IComboBoxTextSubmittedEventArgs,
+    IComboBoxTextSubmittedEventArgs_Vtbl,
+    0x0d7a9794_73b5_585e_bfbb_de6df7eb9fcf
+);
+impl windows_core::RuntimeType for IComboBoxTextSubmittedEventArgs {
+    const SIGNATURE: windows_core::imp::ConstBuffer =
+        windows_core::imp::ConstBuffer::for_interface::<Self>();
+}
+impl windows_core::RuntimeName for IComboBoxTextSubmittedEventArgs {
+    const NAME: &'static str = "Microsoft.UI.Xaml.Controls.IComboBoxTextSubmittedEventArgs";
+}
+pub trait IComboBoxTextSubmittedEventArgs_Impl: windows_core::IUnknownImpl {
+    fn Text(&self) -> windows_core::Result<windows_core::HSTRING>;
+    fn Handled(&self) -> windows_core::Result<bool>;
+    fn SetHandled(&self, value: bool) -> windows_core::Result<()>;
+}
+impl IComboBoxTextSubmittedEventArgs_Vtbl {
+    pub const fn new<Identity: IComboBoxTextSubmittedEventArgs_Impl, const OFFSET: isize>() -> Self
+    {
+        unsafe extern "system" fn Text<
+            Identity: IComboBoxTextSubmittedEventArgs_Impl,
+            const OFFSET: isize,
+        >(
+            this: *mut core::ffi::c_void,
+            result__: *mut *mut core::ffi::c_void,
+        ) -> windows_core::HRESULT {
+            unsafe {
+                let this: &Identity =
+                    &*((this as *const *const ()).offset(OFFSET) as *const Identity);
+                match IComboBoxTextSubmittedEventArgs_Impl::Text(this) {
+                    Ok(ok__) => {
+                        result__.write(core::mem::transmute_copy(&ok__));
+                        core::mem::forget(ok__);
+                        windows_core::HRESULT(0)
+                    }
+                    Err(err) => err.into(),
+                }
+            }
+        }
+        unsafe extern "system" fn Handled<
+            Identity: IComboBoxTextSubmittedEventArgs_Impl,
+            const OFFSET: isize,
+        >(
+            this: *mut core::ffi::c_void,
+            result__: *mut bool,
+        ) -> windows_core::HRESULT {
+            unsafe {
+                let this: &Identity =
+                    &*((this as *const *const ()).offset(OFFSET) as *const Identity);
+                match IComboBoxTextSubmittedEventArgs_Impl::Handled(this) {
+                    Ok(ok__) => {
+                        result__.write(core::mem::transmute_copy(&ok__));
+                        windows_core::HRESULT(0)
+                    }
+                    Err(err) => err.into(),
+                }
+            }
+        }
+        unsafe extern "system" fn SetHandled<
+            Identity: IComboBoxTextSubmittedEventArgs_Impl,
+            const OFFSET: isize,
+        >(
+            this: *mut core::ffi::c_void,
+            value: bool,
+        ) -> windows_core::HRESULT {
+            unsafe {
+                let this: &Identity =
+                    &*((this as *const *const ()).offset(OFFSET) as *const Identity);
+                IComboBoxTextSubmittedEventArgs_Impl::SetHandled(this, value).into()
+            }
+        }
+        Self {
+            base__: windows_core::IInspectable_Vtbl::new::<
+                Identity,
+                IComboBoxTextSubmittedEventArgs,
+                OFFSET,
+            >(),
+            Text: Text::<Identity, OFFSET>,
+            Handled: Handled::<Identity, OFFSET>,
+            SetHandled: SetHandled::<Identity, OFFSET>,
+        }
+    }
+    pub fn matches(iid: &windows_core::GUID) -> bool {
+        iid == &<IComboBoxTextSubmittedEventArgs as windows_core::Interface>::IID
+    }
+}
+#[repr(C)]
+#[doc(hidden)]
+pub struct IComboBoxTextSubmittedEventArgs_Vtbl {
+    pub base__: windows_core::IInspectable_Vtbl,
+    pub Text: unsafe extern "system" fn(
+        *mut core::ffi::c_void,
+        *mut *mut core::ffi::c_void,
+    ) -> windows_core::HRESULT,
+    pub Handled:
+        unsafe extern "system" fn(*mut core::ffi::c_void, *mut bool) -> windows_core::HRESULT,
+    pub SetHandled:
+        unsafe extern "system" fn(*mut core::ffi::c_void, bool) -> windows_core::HRESULT,
 }
 windows_core::imp::define_interface!(
     IContentControl,
